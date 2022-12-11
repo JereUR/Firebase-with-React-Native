c48b3a3168a36cbe0346ee5539cf29597169aa98
Jeremias Dominguez Vega <jeremias.jdv@gmail.com>
2022-12-11 20:31:22 -0300

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,210 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Alert,
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { BlurView } from "expo-blur";
+
+import {
+  getAuth,
+  creteUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase-config";
+import {
+  NavigationContainer,
+  StackActions,
+  useNavigation,
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const uri = "https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg";
+const profilePicture =
+  "https://cdn.discordapp.com/attachments/1051619548576632937/1051641726210687087/user-profile.png";
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function LoginScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  const handleCreateAccount = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log("Account created");
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+        Alert.alert(error.message);
+      });
+  };
+
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log("Signed in!");
+        const user = userCredential.user;
+        console.log(user);
+        navigation.navigate("Home");
+      })
+      .catch((error) => {
+        console.log(error);
+        Alert.alert(error.message);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{ uri }}
+        style={[styles.image, StyleSheet.absoluteFill]}
+      ></Image>
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <BlurView intensity={90}>
+          <View style={styles.login}>
+            <Image
+              source={{ uri: profilePicture }}
+              style={styles.profilePicture}
+            ></Image>
+
+            <View>
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "#fff" }}>
+                E-mail
+              </Text>
+              <TextInput
+                style={styles.input}
+                placeholder="email@example.com"
+                onChangeText={(text) => setEmail(text)}
+              ></TextInput>
+            </View>
+            <View>
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "#fff" }}>
+                Password
+              </Text>
+              <TextInput
+                style={styles.input}
+                placeholder="password"
+                secureTextEntry={true}
+                onChangeText={(text) => setPassword(text)}
+              ></TextInput>
+            </View>
+            <TouchableOpacity
+              onPress={handleSignIn}
+              style={[styles.button, { backgroundColor: "#00CFEB90" }]}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Login
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handleCreateAccount}
+              style={[styles.button, { backgroundColor: "#6792F090" }]}
+            >
+              <Text style={{ fontSize: 17, fontWeight: "400", color: "white" }}>
+                Create Account
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </BlurView>
+      </ScrollView>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
+  },
+
+  login: {
+    width: 350,
+    height: 500,
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: "#fff",
+    borderWidth: 1,
+    marginVertical: 30,
+  },
+
+  input: {
+    width: 250,
+    height: 40,
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ffffff90",
+    marginBottom: 20,
+  },
+
+  button: {
+    width: 250,
+    height: 40,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 10,
+    borderColor: "#fff",
+    borderWidth: 1,
   },
 });
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyArrXFuJliiV6rRr1Qp1LBLP0aYdAkFCP4",
+  authDomain: "test-e4bed.firebaseapp.com",
+  projectId: "test-e4bed",
+  storageBucket: "test-e4bed.appspot.com",
+  messagingSenderId: "331077752982",
+  appId: "1:331077752982:web:587d3411ec2e71336c4ab7",
+};
